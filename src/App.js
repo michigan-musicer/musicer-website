1e489fc15cefac31eb1803d7bed628311d5594fa
musicer <musicer@umich.edu>
2021-08-13 21:18:00 -0400

    initial commit, cleanup and styling fixes neessary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import About from './about/About.js'
+import Portfolio from './portfolio/Portfolio.js'
+import NavigationBar from './navigationBar/NavigationBar.js'
+import './App.css';
+import getAboutProps from './about/getAboutProps.js';
 
-import Main from './Main'
-
-function App() {
+export default function App() {
+// need LHS about me bar, RHS portfolio
+// about me bar should be in em, not percent
+// will need to make it responsive to be viewable on mobile
   return (
-    <div className="App">
-      <Main></Main>
+    // copy the styling for matUI card and use it here
+    <div className='app-container'> 
+      <div className='app-leftside-container'>
+        <About props={getAboutProps()}/>
+      </div>
+      <div className='app-rightside-container'>
+        {/* <NavigationBar/>
+        <Portfolio/> */}
+      </div>
     </div>
   );
 }
-
-export default App;
