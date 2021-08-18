d464a7a3eddc1e18ae5599729dcafb67b9fb7576
musicer <musicer@umich.edu>
2021-08-18 02:33:00 -0400

    mapping succeeded in navbar, need to replicate with portfolio sections next

--- src/navigationBar/NavigationBar.js
+++ src/navigationBar/NavigationBar.js
@@ -1,9 +1,9 @@
 import './NavigationBar.css';
-import Divider from '../util/Divider.js'
 
 export default function NavigationBar(props) {
   let listWithNumItems = [];
-  for (let i = 0; i < props.numItems; ++i) {
+  // should replace with props.props.numItems
+  for (let i = 0; i < 1; ++i) {
     listWithNumItems.push(i);
   }
   
@@ -11,19 +11,14 @@ export default function NavigationBar(props) {
     <div className='navigation-bar-spacing'>
       <div className='navigation-bar-container'>
         <ul className='navigation-bar-list'>
-        {/* // temp before I figure out prop passing */}
-        <li className='navigation-bar-item'>
-          <h2 className='navigation-bar-text'>Portfolio</h2>
-          <hr className='navigation-bar-underline'/>
-        </li>  
-        {/* 
-          {
-            listWithNumItems.map(i =>
-              {
-                <li className='navigation-bar-item'>{props.items[i]}</li>
-              }
-            )
-          } */}
+        {
+          listWithNumItems.map((i) =>
+            <li key={i} className='navigation-bar-item'>
+              {/* should replace with props.props.items[i] */}
+              <h2 className='navigation-bar-text'>Portfolio</h2>
+              <hr className='navigation-bar-underline'/>
+            </li>)
+        }
         </ul>
       </div>
     </div>
