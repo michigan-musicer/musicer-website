e825536d1956c97a60a0bc6784d8abf07d65ba76
musicer <musicer@umich.edu>
2021-09-02 00:08:14 -0400

    rematch website w/ current CV

--- src/portfolio/Projects.js
+++ src/portfolio/Projects.js
@@ -18,7 +18,7 @@ export default function Projects() {
                 title: 'Details', 
                 items: [
                     // turn these into items with left and rights -- use icons on the right to indicate what tools you used / worked with
-                    {item: <p>Wrote over 4,700 lines of code contributing to the delivery of three major components and the utility library on a distributed, multithreaded, C++ search engine totaling over 16,000 lines of code, seven million documents indexed, and three TB of collected data split across over 30 machines.</p>},
+                    {item: <p>Wrote over 4,700 lines of code contributing to the delivery of three major components and the utility library on a distributed, multithreaded search engine written in C++ totaling over 16,000 lines of code, seven million documents indexed, and three terabytes of collected data split across more than 30 machines.</p>},
                     {item: <p>Cooperated with a six-person team by creating agendas, communicating about assignments, and pair programming on different components to work more efficiently and finish a project not doable by a single individual.</p>},
                     {item: <p>Designed, coded, and integrated a solution for a distributed system problem that had previously never been solved in the class.</p>},
                   ], 
--- src/portfolio/WorkExp.js
+++ src/portfolio/WorkExp.js
@@ -10,18 +10,17 @@ export default function WorkExp() {
           // <div style={{width: '100%'}}>
           <div style={{width: '100%'}}>
             <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-              <p style={{paddingRight: '2em'}}><b>Software engineer, full stack</b> - Ford Autonomous Vehicles LLC</p>
+              <p style={{paddingRight: '2em'}}><b>Full stack software engineer</b> - Ford Autonomous Vehicles LLC</p>
               <em>May 2021 - August 2021</em>
             </div>
             <DropDown props={{title: 'Accomplishments at Ford', items: 
               [
                 // turn these into items with left and rights -- use icons on the right to indicate what tools you used / worked with
-                {item: <p>Expanded demo product with single AWS Lambda endpoint and single frontend page into an independent product with five Lambda endpoints, Spring Boot services integrated with DynamoDB and Kafka, and three React pages.</p>},
-                {item: <p>Created resources on the AWS cloud platform, including DynamoDB tables, Spring Boot services running on Elastic Container Service, and Lambda endpoints to expand the infrastructure of the application and allow the addition of future features and functionality.</p>},
-                {item: <p>Added two pages and corresponding backend infrastructure (Spring Boot backend, Kafka consumer, AWS Lambda endpoints) to showcase new business information to our customers.</p>},
+                {item: <p>Expanded demo idea with single endpoint and single frontend page into a standalone product by adding four AWS Lambda endpoints, two DynamoDB tables, a Spring Boot backend hosted on AWS Elastic Container Service running a Kafka consumer, and two React pages to add business functionality and display new business information to customers.</p>},
+                {item: <p>Practiced agile and pair programming workflows with the assistance of tools including Rally, Jenkins, and SonarQube, completing 50 user stories as a two-developer team in three months.</p>},
+                {item: <p>Read from third-party APIs from Autonomic and cooperated with external software engineers to display relevant business data to customers.</p>},
                 {item: <p>Fixed bugs, refactored existing code, and improved performance by restructuring our application and reducing the amount of API calls,  ingraining best software engineering practices along the way.</p>},
                 {item: <p>Interviewed customers about web application design and implemented changes based on feedback to display data to consumers of our product in a more helpful manner.</p>},
-                {item: <p>Practiced agile and pair programming workflows with the assistance of tools including Rally, Jenkins, and SonarQube, completing 50 user stories as a two-developer team in three months. </p>},                
               ], indent: false}}/>
           
           </div>, 
@@ -31,14 +30,14 @@ export default function WorkExp() {
         left: 
           <div style={{width: '100%'}}>
              <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-              <p style={{paddingRight: '2em'}}><b>Student researcher</b> - UMTRI (University of Michigan Transportation Research Institute)</p>
+              <p style={{paddingRight: '2em'}}><b>Student research leader</b> - UMTRI (University of Michigan Transportation Research Institute)</p>
               <em>May 2020 - August 2020</em>
             </div>
             <DropDown props={
               {title: 'Accomplishments at UMTRI', items: [
-                {item: <p>Created plans, Python code, tests, and follow-up analysis for utilizing algorithms from research papers  to classify images without supervision, laying foundations for future approaches with similar strategies.</p>},
-                {item: <p>Investigated computer vision methods to reduce dataset size to increase efficiency of video labelers while maximizing accuracy and minimizing biased decision-making, facilitating research by our funder, the Federal Highway Administration.</p>},
-                {item: <p>Introduced new student researchers to the lab by writing documentation and holding meetings, familiarizing newcomers with the project three months faster than it took for previous student researchers. </p>},
+                {item: <p>Created plans, Python code, tests, and follow-up analysis for utilizing algorithms from research papers to classify images without supervision, laying foundations for future approaches with similar strategies.</p>},
+                {item: <p>Investigated computer vision methods to reduce dataset size with the goal of increasing efficiency, maximizing accuracy, and minimizing biased decision-making in data processing, facilitating research by other transportation researchers.</p>},
+                {item: <p>Introduced new student researchers to the lab by writing documentation and holding meetings, familiarizing newcomers with the project three months faster than it took for previous student researchers.</p>},
               ],
               indent: false
             }}/>
